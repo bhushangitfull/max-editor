d7f131048af56b5f41b7b3dc38ae01d842882c32
kbdev24 <kbdev24@gmail.com>
2025-04-02 07:10:09 +0000

    shadcn initialized

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Box } from "@chakra-ui/react";
 import CodeEditor from "./components/CodeEditor.jsx";
-
 function App() {
   return (
     <Box minH="100vh" bg="#0f0a19" color="gray.500" px={6} py={8}>
